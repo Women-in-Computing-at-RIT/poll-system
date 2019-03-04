3b86b4c7395ca198da877dda54b3f734f8974b69
pdfkpb <pdfkpb@gmail.com>
2019-03-04 18:34:02 -0500

    starts auth, and utilizes jwt

--- ep_auth.py
+++ ep_auth.py
@@ -3,3 +3,38 @@ File::ep_auth.py
 Author::Kevin.P.Barnett
 Date::Feb.25.2019
 """
+
+from flask import Blueprint, request
+from .tokens import generate_token_with_claims, validate_token
+from .db import get_db
+
+import json
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+@bp.route('/new', methods=['POST'])
+def post_new_user():
+    pass
+
+
+@bp.route('/login', methods=['POST'])
+def post_login():
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+
+    if username is None or password is None:
+        return json.dumps({'msg': 'Either username or password is not entered'}), 400
+
+    return_data = {
+        'msg': 'Successfully logged in',
+        'token': generate_token_with_claims({'user_id': 'test'})
+    }
+
+    return json.dumps(return_data), 200
+
+
+@bp.route('/', methods=['logout'])
+@validate_token
+def post_logout():
+    pass
--- tokens.py
+++ tokens.py
@@ -1,19 +1,35 @@
 """
-File::token_validation.py
+File::tokens.py
 Author::Kevin.P.Barnett
 Date::Feb.25.2019
 """
 
-from jwt import decode
+from jwt import (
+    encode,
+    decode,
+    ExpiredSignatureError,
+    InvalidSignatureError
+)
 from os import environ
+from datetime import datetime, timedelta
 
-def validate_token(func, token):
-    def decorator():
-        #Before Function
+
+def generate_token_with_claims(claims):
+    claims['iss'] = 'polls.wic.rit.edu'
+    claims['iat'] = datetime.utcnow()
+    claims['exp'] = datetime.utcnow() + timedelta(days=31)
+
+    return encode(claims, environ['POLLS_SECRET'], algorithm='HS256')
+
+
+def validate_token(func):
+    def decorator(token):
         try:
             decode(token, environ['POLLS_SECRET'], algorithms=['HS256'])
-        except 
-
-        func()
+            func()
+        except ExpiredSignatureError:
+            print('Token Expired')
+        except InvalidSignatureError:
+            print('Token Modified')
 
     return decorator
